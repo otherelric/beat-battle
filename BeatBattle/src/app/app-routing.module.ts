ddc9713520d18b5d2e42377a41d2a30674f72134
Tim Ascencio <ascenciot123@gmail.com>
2018-12-15 23:44:28 -0500

    css the crazy!
    nav fully implemented visually
    
    components are laid out

--- BeatBattle/src/app/app-routing.module.ts
+++ BeatBattle/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/app/components/login/login.component';
+import { BattleComponent } from 'src/app/components/battle/battle.component';
+import { PlaylistComponent } from 'src/app/components/playlist/playlist.component';
+import { ExploreComponent } from 'src/app/components/explore/explore.component';
 
 const routes: Routes = [
+  { path: 'explore', component: ExploreComponent },
+  { path: 'battle', component: BattleComponent },
+  { path: 'playlist', component: PlaylistComponent },
   { path: 'login', component: LoginComponent }
 ];
 
--- BeatBattle/src/app/app.module.ts
+++ BeatBattle/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { BattleComponent } from './components/battle/battle.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { ExploreComponent } from './components/explore/explore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BattleComponent,
+    PlaylistComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
